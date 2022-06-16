57fc13e61cdba8c6826123624e0f74ddcdb5a513
YutaK1026 <inheritans1026@gmail.com>
2022-06-16 17:10:02 +0900

    FieldPage

--- frontend/src/views/FieldPage.vue
+++ frontend/src/views/FieldPage.vue
@@ -1,11 +1,132 @@
 <template>
   <v-container>
-    <p>デュエル！</p>
+    <div class="field">
+      <VueDrag
+        v-model="selecteddata"
+        group="myGroup"
+        @start="drag=true"
+        @end="drag=false"
+        :options="options"
+        class="area"
+      >
+      <div
+        v-for="select in selecteddata"
+        :key="select.id"
+        class="item"
+      >
+        <v-card
+          height="200px"
+          hover
+        >{{ select.name }}
+        </v-card>
+      </div>
+      </VueDrag>
+    </div>
+      <VueDrag
+        v-model="mydata"
+        group="myGroup"
+        @start="drag=true"
+        @end="drag=false"
+        :options="options"
+        class="area"
+      >
+        <div
+          v-for="mine in mydata"
+          :key="mine.id"
+          class="item"
+        >
+          <v-card
+            height="200px"
+            hover
+          >{{ mine.name }}
+          </v-card>
+        </div>
+      </VueDrag>
   </v-container>
 </template>
 
 <script>
+import VueDrag from 'vuedraggable'
+
   export default {
     name: 'field',
+    components: {
+      VueDrag
+    },
+    data (){
+      return {
+        options: {
+          group: "myGroup",
+          animation: 200
+        },
+        selecteddata: [
+        ],
+        mydata: [
+          {
+            id: 1,
+            name: "C",
+            type: "language",
+            value: 50
+          },
+          {
+            id: 2,
+            name: "C++",
+            type: "language",
+            value: 60
+          },
+          {
+            id: 3,
+            name: "C#",
+            type: "language",
+            value: 50
+          },
+          {
+            id: 4,
+            name: "python",
+            type: "language",
+            value: 20
+          },
+          {
+            id: 5,
+            name: "Java",
+            type: "language",
+            value: 50
+          },
+          {
+            id: 6,
+            name: "R",
+            type: "language",
+            value: 20
+          },
+        ]
+      }
+    }
   }
 </script>
+
+<style scoped>
+.field {
+  height: 300px;
+  width: 100%;
+  background-color: aquamarine;
+}
+
+.item {
+  display: inline-block;
+  margin: 10px;
+  width: 12%;
+}
+
+.item:hover {
+  cursor: grab;
+}
+
+.item:active{
+  cursor: grabbing;
+}
+
+.area {
+  width: 100%;
+  height: 400px;
+}
+</style>
