f9b2939d731d433d4404ac451c446159ff6a64ae
yutteee <yusaku_nakamura_aa@icloud.com>
2022-06-16 16:49:55 +0900

    vue.draggableでドラックアンドドロップを実装

--- frontend/src/views/FieldPage.vue
+++ frontend/src/views/FieldPage.vue
@@ -1,67 +1,132 @@
 <template>
   <v-container>
-    <v-row>
-      <FieldCard
-        v-for="sample in sampledata"
-        :key="sample.id"
-        :id = sample.id
-        :name = sample.name
-        :type = sample.type
-        :attack = sample.attack
-      />
-    </v-row>
+    <div class="field">
+      <VueDrag
+        v-model="selecteddata"
+        group="myGroup"
+        @start="drag=true"
+        @end="drag=false"
+        :options="options"
+        class="area"
+      >
+      <div
+        v-for="select in selecteddata"
+        :key="select.id"
+        class="item"
+      >
+        <v-card
+          height="200px"
+          hover
+        >{{ select.name }}
+        </v-card>
+      </div>
+      </VueDrag>
+    </div>
+      <VueDrag
+        v-model="mydata"
+        group="myGroup"
+        @start="drag=true"
+        @end="drag=false"
+        :options="options"
+        class="area"
+      >
+        <div
+          v-for="mine in mydata"
+          :key="mine.id"
+          class="item"
+        >
+          <v-card
+            height="200px"
+            hover
+          >{{ mine.name }}
+          </v-card>
+        </div>
+      </VueDrag>
   </v-container>
 </template>
 
 <script>
-import FieldCard from "../components/FieldCard.vue"
+import VueDrag from 'vuedraggable'
 
   export default {
     name: 'field',
     components: {
-      FieldCard
+      VueDrag
     },
     data (){
       return {
-        sampledata: [
+        options: {
+          group: "myGroup",
+          animation: 200
+        },
+        selecteddata: [
+        ],
+        mydata: [
           {
             id: 1,
             name: "C",
             type: "language",
-            attack: 50
+            value: 50
           },
           {
             id: 2,
             name: "C++",
             type: "language",
-            attack: 60
+            value: 60
           },
           {
             id: 3,
             name: "C#",
             type: "language",
-            attack: 50
+            value: 50
           },
           {
             id: 4,
             name: "python",
             type: "language",
-            attack: 20
+            value: 20
           },
           {
             id: 5,
             name: "Java",
             type: "language",
-            attack: 50
+            value: 50
           },
           {
             id: 6,
             name: "R",
             type: "language",
-            attack: 20
+            value: 20
           },
         ]
       }
     }
   }
 </script>
+
+<style scoped>
+.field {
+  height: 300px;
+  width: 100%;
+  background-color: aquamarine;
+}
+
+.item {
+  display: inline-block;
+  margin: 10px;
+  width: 12%;
+}
+
+.item:hover {
+  cursor: grab;
+}
+
+.item:active{
+  cursor: grabbing;
+}
+
+.area {
+  width: 100%;
+  height: 400px;
+}
+</style>
