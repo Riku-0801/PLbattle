cb16b11a7a140fa31d279f542f9a6da85a93a05d
Riko_YAMADA <84134634+akaishikiso@users.noreply.github.com>
2022-06-18 18:16:54 +0900

    現状プッシュ

--- frontend/src/components/FieldCard.vue
+++ frontend/src/components/FieldCard.vue
@@ -1,42 +1,29 @@
 <template>
-<v-hover
-  v-slot="{ hover }"
->
-  <v-col
-    cols="2"
-  >
-    <v-card
-      height="200px"
-      hover
-      :img="require('../assets/logo.png')"
-    >
-    </v-card>
+  <v-hover v-slot="{ hover }">
+    <v-col cols="2">
+      <v-card
+        width="200px"
+        height="232.5px"
+        hover
+        :img="require('../assets/cards/BootStrap.png')"
+      >
+      </v-card>
       <!-- hover時に表示 -->
       <v-expand-transition>
-        <div
-          v-if="hover"
-          width="100"
-        >
+        <div v-if="hover" width="100" color="rgba(0, 18, 42)">
           <p>{{ name }}</p>
           <p>type: {{ type }}</p>
           <p>attack: {{ attack }}</p>
         </div>
       </v-expand-transition>
-  </v-col>
-</v-hover>
+    </v-col>
+  </v-hover>
 </template>
 
 <script>
 export default {
-  props:[
-    'id',
-    'name',
-    'type',
-    'attack'
-  ]
-}
+  props: ["id", "name", "type", "attack"],
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- frontend/src/views/FieldPage.vue
+++ frontend/src/views/FieldPage.vue
@@ -1,67 +1,73 @@
 <template>
-  <v-container>
+  <v-container fluid="false" class="bg">
     <v-row>
       <FieldCard
         v-for="sample in sampledata"
         :key="sample.id"
-        :id = sample.id
-        :name = sample.name
-        :type = sample.type
-        :attack = sample.attack
+        :id="sample.id"
+        :name="sample.name"
+        :type="sample.type"
+        :attack="sample.attack"
       />
     </v-row>
   </v-container>
 </template>
 
 <script>
-import FieldCard from "../components/FieldCard.vue"
+import FieldCard from "../components/FieldCard.vue";
 
-  export default {
-    name: 'field',
-    components: {
-      FieldCard
-    },
-    data (){
-      return {
-        sampledata: [
-          {
-            id: 1,
-            name: "C",
-            type: "language",
-            attack: 50
-          },
-          {
-            id: 2,
-            name: "C++",
-            type: "language",
-            attack: 60
-          },
-          {
-            id: 3,
-            name: "C#",
-            type: "language",
-            attack: 50
-          },
-          {
-            id: 4,
-            name: "python",
-            type: "language",
-            attack: 20
-          },
-          {
-            id: 5,
-            name: "Java",
-            type: "language",
-            attack: 50
-          },
-          {
-            id: 6,
-            name: "R",
-            type: "language",
-            attack: 20
-          },
-        ]
-      }
-    }
-  }
+export default {
+  name: "field",
+  components: {
+    FieldCard,
+  },
+  data() {
+    return {
+      sampledata: [
+        {
+          id: 1,
+          name: "C",
+          type: "language",
+          attack: 50,
+        },
+        {
+          id: 2,
+          name: "C++",
+          type: "language",
+          attack: 60,
+        },
+        {
+          id: 3,
+          name: "C#",
+          type: "language",
+          attack: 50,
+        },
+        {
+          id: 4,
+          name: "python",
+          type: "language",
+          attack: 20,
+        },
+        {
+          id: 5,
+          name: "Java",
+          type: "language",
+          attack: 50,
+        },
+        {
+          id: 6,
+          name: "R",
+          type: "language",
+          attack: 20,
+        },
+      ],
+    };
+  },
+};
 </script>
+<style scoped>
+.bg {
+  background-color: rgba(0, 18, 42);
+  height: 100vh;
+}
+</style>
