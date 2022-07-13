a7aaca48b5297967402fa42df9b0f63188d7074c
maron <riku20020801@gmail.com>
2022-07-13 20:21:53 +0900

    origin変更

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: "https://code--duel.herokuapp.com/",
     methods: ["GET", "POST"],
   },
 });
